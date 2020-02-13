4a6df975db7a41e572dd33810dfca351601aa5fe
ferdinand <trendelenburger19.04@googlemail.com>
2020-02-13 13:43:06 +0100

    Made a GUI

--- src/trendelenburg/UI/Window.java
+++ src/trendelenburg/UI/Window.java
@@ -26,14 +26,33 @@ public class Window extends JFrame {
     JTextField suche = new JTextField();
 
     JLabel labelAuswahl = new JLabel("AUSWAHL", JLabel.CENTER);
+
+    JLabel labelAnswer = new JLabel("Antwort:", JLabel.CENTER);
     JTextField textFieldAnswer = new JTextField();
+    JButton buttonCheck = new JButton("abgeben");
+    JButton buttonRight = new JButton("anzeigen");
+
+    JLabel labelLegende = new JLabel("Legende:", JLabel.CENTER);
+
+    JLabel labelOutput = new JLabel("Check:", JLabel.CENTER);
+    JLabel labelOutputStatus = new JLabel();
+
+    JLabel labelOption1 = new JLabel("Richtig:", JLabel.CENTER);
+    JLabel labelOption1COLOR = new JLabel();
+
+    JLabel labelOption2 = new JLabel("Richtig dabei:", JLabel.CENTER);
+    JLabel labelOption2COLOR = new JLabel();
+
+    JLabel labelOption3 = new JLabel("Falsch:", JLabel.CENTER);
+    JLabel labelOption3COLOR = new JLabel();
 
 
 
     public Window() throws HeadlessException {
         super("Hallo Thommi");
+        setResizable(false);
         setLocationRelativeTo(null);
-        setSize(900, 600);
+        setSize(550, 390);
 
         Utils.readPropertys();
         karten = Data.readFromFileAsList(Utils.dataFile);
@@ -57,6 +76,11 @@ public class Window extends JFrame {
                 public void actionPerformed(ActionEvent actionEvent) {
                     labelAuswahl.setText(jbutton.getText());
                     labelAuswahl.setForeground(Color.GREEN);
+                    labelOutputStatus.setOpaque(true);
+                    labelOutputStatus.setBackground(Color.WHITE);
+                    labelOutputStatus.setText("");
+                    textFieldAnswer.setText("");
+
                 }
             });
         }
@@ -68,7 +92,19 @@ public class Window extends JFrame {
     private void zuweisen() {
         add(suche);
         add(labelAuswahl);
+        add(labelAnswer);
         add(textFieldAnswer);
+        add(buttonCheck);
+        add(buttonRight);
+        add(labelOutput);
+        add(labelOutputStatus);
+        add(labelLegende);
+        add(labelOption1);
+        add(labelOption1COLOR);
+        add(labelOption2);
+        add(labelOption2COLOR);
+        add(labelOption3);
+        add(labelOption3COLOR);
         add(js);
         add(labelNix);
     }
@@ -104,20 +140,117 @@ public class Window extends JFrame {
         labelAuswahl.setBackground(Color.BLACK);
         labelAuswahl.setOpaque(true);
 
-        labelAuswahl.setSize(180, 30);
+        labelAuswahl.setSize(245, 30);
         labelAuswahl.setLocation(270, 40);
         labelAuswahl.setForeground(Color.RED);
         labelAuswahl.setBorder(new LineBorder(Color.BLACK));
         labelAuswahl.setVisible(true);
 
-        textFieldAnswer.setSize(180, 30);
-        textFieldAnswer.setLocation(270, 75);
+        labelAnswer.setSize(120, 30);
+        labelAnswer.setLocation(270, 75);
+        labelAnswer.setHorizontalAlignment(JTextField.RIGHT);
+        labelAnswer.setVisible(true);
+
+        textFieldAnswer.setSize(120, 30);
+        textFieldAnswer.setLocation(395, 75);
         textFieldAnswer.setBorder(new LineBorder(Color.BLACK));
         textFieldAnswer.setHorizontalAlignment(JTextField.CENTER);
         textFieldAnswer.setVisible(true);
+        textFieldAnswer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                checkIfRight();
+            }
+        });
+
+        labelOutput.setSize(120, 30);
+        labelOutput.setLocation(270, 110);
+        labelOutput.setHorizontalAlignment(JTextField.RIGHT);
+        labelOutput.setVisible(true);
+
+        labelOutputStatus.setSize(120, 30);
+        labelOutputStatus.setLocation(395, 110);
+        labelOutputStatus.setBorder(new LineBorder(Color.BLACK));
+        labelOutputStatus.setHorizontalAlignment(JTextField.CENTER);
+        labelOutputStatus.setVisible(true);
+
+        buttonRight.setSize(120, 30);
+        buttonRight.setLocation(270, 145);
+        buttonRight.setBackground(Color.GRAY);
+        buttonRight.setBorder(new LineBorder(Color.BLACK));
+        buttonRight.setVisible(true);
+        buttonRight.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                for (int i = 0; i < karten.size(); i++) {
+                    if(karten.get(i).getFrage().equals(labelAuswahl.getText())) {
+                        String out = textFieldAnswer.getText() + " (" + karten.get(i).getAntwort() + ")";
+                        if(out.length()>10){
+                            out = textFieldAnswer.getText().substring(0, 6) + "... (" + karten.get(i).getAntwort() + ")";
+                        }
+                        labelOutputStatus.setText(out);
+                    }
+
+                }
+            }
+        });
+
+        buttonCheck.setSize(120, 30);
+        buttonCheck.setLocation(395, 145);
+        buttonCheck.setVisible(true);
+        buttonCheck.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                checkIfRight();
+            }
+        });
+
+        labelLegende.setSize(120, 30);
+        labelLegende.setLocation(395, 225);
+        labelLegende.setHorizontalAlignment(JTextField.RIGHT);
+        labelLegende.setVisible(true);
+
+        labelOption1.setSize(120, 30);
+        labelOption1.setLocation(270, 250);
+        labelOption1.setHorizontalAlignment(JTextField.RIGHT);
+        labelOption1.setVisible(true);
+
+        labelOption1COLOR.setSize(120, 30);
+        labelOption1COLOR.setLocation(395, 250);
+        labelOption1COLOR.setOpaque(true);
+        labelOption1COLOR.setBackground(Color.GREEN);
+        labelOption1COLOR.setBorder(new LineBorder(Color.BLACK));
+        labelOption1COLOR.setHorizontalAlignment(JTextField.CENTER);
+        labelOption1COLOR.setVisible(true);
+
+        labelOption2.setSize(120, 30);
+        labelOption2.setLocation(270, 285);
+        labelOption2.setHorizontalAlignment(JTextField.RIGHT);
+        labelOption2.setVisible(true);
+
+        labelOption2COLOR.setSize(120, 30);
+        labelOption2COLOR.setLocation(395, 285);
+        labelOption2COLOR.setOpaque(true);
+        labelOption2COLOR.setBackground(Color.YELLOW);
+        labelOption2COLOR.setBorder(new LineBorder(Color.BLACK));
+        labelOption2COLOR.setHorizontalAlignment(JTextField.CENTER);
+        labelOption2COLOR.setVisible(true);
+
+        labelOption3.setSize(120, 30);
+        labelOption3.setLocation(270, 320);
+        labelOption3.setHorizontalAlignment(JTextField.RIGHT);
+        labelOption3.setVisible(true);
+
+        labelOption3COLOR.setSize(120, 30);
+        labelOption3COLOR.setLocation(395, 320);
+        labelOption3COLOR.setOpaque(true);
+        labelOption3COLOR.setBackground(Color.RED);
+        labelOption3COLOR.setBorder(new LineBorder(Color.BLACK));
+        labelOption3COLOR.setHorizontalAlignment(JTextField.CENTER);
+        labelOption3COLOR.setVisible(true);
 
         js = new JScrollPane(labelFragen);
-        js.setSize(230, 520);
+        js.setSize(230, 310);
         js.setLocation(20, 40);
         js.setVisible(true);
         labelFragen.setVisible(true);
@@ -163,6 +296,31 @@ public class Window extends JFrame {
         componentenRegistrieren();
         zuweisen();
         setVisible(true);
+    }
+
+    private void checkIfRight(){
+        labelOutputStatus.setText("");
+        String[] input = textFieldAnswer.getText().replace(" ", "").split(",");
+        for (int i = 0; i < karten.size(); i++) {
+            if(karten.get(i).getFrage().equals(labelAuswahl.getText())) {
+                for (int j = 0; j < input.length; j++) {
+                    if(karten.get(i).getAntwort().toLowerCase().equals(input[j].toLowerCase())) {
+                        if(input.length>1) {
+                            labelOutputStatus.setBackground(Color.YELLOW);
+                            labelOutputStatus.setOpaque(true);
+                        }else {
+                            labelOutputStatus.setText(textFieldAnswer.getText());
+                            labelOutputStatus.setBackground(Color.GREEN);
+                            labelOutputStatus.setOpaque(true);
+                        }
+                        break;
+                    }else{
+                        labelOutputStatus.setBackground(Color.RED);
+                        labelOutputStatus.setOpaque(true);
+                    }
+                }
+            }
+        }
     }
 
 }
